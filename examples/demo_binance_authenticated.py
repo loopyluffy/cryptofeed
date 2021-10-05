ff9cf87c67010a2791dc7c6bcbc6aaa88a0c9718
loopyluffy <luffy@loopystory.com>
2021-10-05 14:57:39 +0900

    update demo_binance_authenticated.py for testing...

--- examples/demo_binance_authenticated.py
+++ examples/demo_binance_authenticated.py
@@ -1,3 +1,18 @@
+
+# import cryptofeed directly from local directory @logan
+import os
+import sys
+cur_dir_name = os.path.dirname(__file__)
+parent_dir_path = os.path.dirname(cur_dir_name)
+sys.path.append(parent_dir_path)
+# repo_dir_path = os.path.dirname(parent_dir_path)
+# cryptofeed_dir_path = repo_dir_path + '/cryptofeed@loopyluffy'
+# sys.path.append(cryptofeed_dir_path)
+
+# to import pyx file @logan
+import pyximport
+pyximport.install()
+
 from cryptofeed import FeedHandler
 from cryptofeed.defines import BALANCES, POSITIONS
 from cryptofeed.exchanges import Binance, BinanceDelivery, BinanceFutures
@@ -12,19 +27,20 @@ async def position(**kwargs):
 
 
 def main():
-    path_to_config = 'config.yaml'
+    # path_to_config = 'config.yaml'
+    path_to_config = 'sandbox/rest_config.yaml'
 
-    binance = Binance(config=path_to_config, subscription={BALANCES: []}, timeout=-1, callbacks={BALANCES: balance})
-    binance_delivery = BinanceDelivery(config=path_to_config, subscription={BALANCES: [], POSITIONS: []}, timeout=-1, callbacks={BALANCES: balance, POSITIONS: position})
+    # binance = Binance(config=path_to_config, subscription={BALANCES: []}, timeout=-1, callbacks={BALANCES: balance})
+    # binance_delivery = BinanceDelivery(config=path_to_config, subscription={BALANCES: [], POSITIONS: []}, timeout=-1, callbacks={BALANCES: balance, POSITIONS: position})
     binance_futures = BinanceFutures(config=path_to_config, subscription={BALANCES: [], POSITIONS: []}, timeout=-1, callbacks={BALANCES: balance, POSITIONS: position})
 
-    print(binance._generate_token())
-    print(binance_delivery._generate_token())
+    # print(binance._generate_token())
+    # print(binance_delivery._generate_token())
     print(binance_futures._generate_token())
 
     f = FeedHandler()
-    f.add_feed(binance)
-    f.add_feed(binance_delivery)
+    # f.add_feed(binance)
+    # f.add_feed(binance_delivery)
     f.add_feed(binance_futures)
     f.run()
 
